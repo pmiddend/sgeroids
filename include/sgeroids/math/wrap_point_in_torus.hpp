316d1ba083ac7bde1ab8e3aebad4b19b91ff5459
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-08-28 20:24:11 +0200

    Fix for fcppt

--- include/sgeroids/math/wrap_point_in_torus.hpp
+++ include/sgeroids/math/wrap_point_in_torus.hpp
@@ -2,25 +2,27 @@
 #define SGEROIDS_MATH_WRAP_POINT_IN_TORUS_HPP_INCLUDED
 
 #include <fcppt/make_int_range_count.hpp>
+#include <fcppt/math/size_type.hpp>
 #include <fcppt/math/box/object_impl.hpp>
 #include <fcppt/math/vector/object_impl.hpp>
+#include <fcppt/math/vector/static.hpp>
 
 
 namespace sgeroids
 {
 namespace math
 {
-template<typename T,fcppt::math::size_type N,typename VN,typename S>
-fcppt::math::vector::object<T,VN,S> const
+template<typename T,fcppt::math::size_type N,typename S>
+fcppt::math::vector::static_<T,N>
 wrap_point_in_torus(
-	fcppt::math::vector::object<T,VN,S> p,
+	fcppt::math::vector::object<T,N,S> p,
 	fcppt::math::box::object<T,N> const &b)
 {
 	for(
 		auto const i
 		:
 		fcppt::make_int_range_count(
-			VN::value
+			N
 		)
 	)
 	{
